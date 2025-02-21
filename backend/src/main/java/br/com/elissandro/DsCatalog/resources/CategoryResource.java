009d986c3d0f0da142f8a689b7c6c4de3159f4f8
elissandroa <elissandroa@hotmail.com>
2025-02-21 19:04:48 -0300

    Find Category by id, exception handling

--- backend/src/main/java/br/com/elissandro/DsCatalog/resources/CategoryResource.java
+++ backend/src/main/java/br/com/elissandro/DsCatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class CategoryResource {
 	public ResponseEntity<List<CategoryDTO>> findAll() {
 		List<CategoryDTO> cat = service.findAll();
 		return ResponseEntity.ok().body(cat);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findBydId(@PathVariable Long id){
+		CategoryDTO cat = service.findByID(id);
+		return ResponseEntity.ok().body(cat);	
 	}
 }
---
+++ backend/src/main/java/br/com/elissandro/DsCatalog/resources/exceptions/ResourcesExceptionHandler.java
@@ -0,0 +1,28 @@
+package br.com.elissandro.DsCatalog.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.elissandro.DsCatalog.services.exceptions.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourcesExceptionHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandarError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandarError err = new StandarError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
