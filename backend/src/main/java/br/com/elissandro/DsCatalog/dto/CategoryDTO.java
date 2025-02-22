c60c0fc7daeae8b8884f7b0181745d0b052d44d5
elissandroa <elissandroa@hotmail.com>
2025-02-22 17:52:28 -0300

    Pagination

--- backend/src/main/java/br/com/elissandro/DsCatalog/dto/CategoryDTO.java
+++ backend/src/main/java/br/com/elissandro/DsCatalog/dto/CategoryDTO.java
@@ -1,15 +1,11 @@
 package br.com.elissandro.DsCatalog.dto;
 
-import java.time.Instant;
-
 import br.com.elissandro.DsCatalog.entities.Category;
 
 public class CategoryDTO {
 
 	private Long id;
 	private String name;
-	private Instant createAt;
-	private Instant updateAt;
 
 	public CategoryDTO() {
 	}
@@ -22,8 +18,6 @@ public class CategoryDTO {
 	public CategoryDTO(Category entity) {
 		id = entity.getId();
 		name = entity.getName();
-		createAt = entity.getCreatedAt();
-		updateAt = entity.getUpdatedAt();
 	}
 
 	public Long getId() {
@@ -41,14 +35,5 @@ public class CategoryDTO {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public Instant getCreateAt() {
-		return createAt;
-	}
-
-	public Instant getUpdateAt() {
-		return updateAt;
-	}
-	
 	
 }
