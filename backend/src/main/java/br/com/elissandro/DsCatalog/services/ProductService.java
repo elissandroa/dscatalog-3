f60d569e7aa60d1efa600e435e2242c807967d30
elissandroa <elissandroa@hotmail.com>
2025-02-23 16:30:03 -0300

    Product insert, update, delete

--- backend/src/main/java/br/com/elissandro/DsCatalog/services/ProductService.java
+++ backend/src/main/java/br/com/elissandro/DsCatalog/services/ProductService.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.com.elissandro.DsCatalog.dto.ProductDTO;
 import br.com.elissandro.DsCatalog.entities.Product;
+import br.com.elissandro.DsCatalog.repositories.CategoryRepository;
 import br.com.elissandro.DsCatalog.repositories.ProductRepository;
 import br.com.elissandro.DsCatalog.services.exceptions.DatabaseException;
 import jakarta.persistence.EntityNotFoundException;
@@ -22,43 +23,46 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
-	 Page<Product> list = repository.findAll(pageRequest);
-	 return list.map(x -> new ProductDTO(x));
+		Page<Product> list = repository.findAll(pageRequest);
+		return list.map(x -> new ProductDTO(x));
 	}
-	
+
 	@Transactional(readOnly = true)
 	public ProductDTO findByID(Long id) {
 		Optional<Product> obj = repository.findById(id);
 		Product entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
-		return new ProductDTO(entity, entity.getCategories());
+		return new ProductDTO(entity);
 	}
-	
+
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
-		return new ProductDTO(entity);
+		return new ProductDTO(entity, entity.getCategories());
 	}
-	
+
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 			Product entity = repository.getReferenceById(id);
-			
+			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
-			return new ProductDTO(entity);
+			return new ProductDTO(entity, entity.getCategories());
 		} catch (EntityNotFoundException e) {
 			throw new EntityNotFoundException("Id not found " + id);
 		}
 	}
-	
+
 	@Transactional(propagation = Propagation.SUPPORTS)
 	public void delete(Long id) {
-		
-		if(!repository.existsById(id)) {
+
+		if (!repository.existsById(id)) {
 			throw new EntityNotFoundException("Id not found " + id);
 		} else {
 			try {
@@ -67,5 +71,18 @@ public class ProductService {
 				throw new DatabaseException("");
 			}
 		}
+	}
+
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setPrice(dto.getPrice());
+		entity.setDescription(dto.getDescription());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setDate(dto.getDate());
+
+		entity.getCategories().clear();
+		dto.getCategories().forEach(catDto -> {
+			entity.getCategories().add(categoryRepository.getReferenceById(catDto.getId()));
+		});
 	}
 }
